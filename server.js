4444867e5819d86648531c7d1627400a3c7443e1
B.J. Allen <ballen@numerex.com>
2014-07-23 19:27:59 -0500

    add semicolons
    been writing too much ruby lately

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var gate = null;
 http.createServer(function(request, response) {
 
   if (request.url == "/check") {
-    response.write("The server is running.")
+    response.write("The server is running.");
   }
   else if (request.url == "/run") {
     if (request.method != "POST") {
-      response.write("POST is required.")
+      response.write("POST is required.");
       response.statusCode = 405;
     }
     else {
